238f567b5593063ccdea9de9f83bf98fca94cb09
Yerim Hu <yerimhu88@gmail.com>
2017-10-31 19:42:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
-var playlist = {key : 'value'};
+var playlist = {artistName : songTitle};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+    playlist.assign({artistName: songTitle})
+    return playlist
+}
